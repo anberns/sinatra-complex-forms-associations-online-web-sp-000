b3fe5c4bbb44f72dac40cb9a507485c5f36b4550
Aaron Berns <anberns@gmail.com>
2019-01-30 21:03:50 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,8 +12,8 @@ class PetsController < ApplicationController
 
   post '/pets' do 
     @pet = Pet.create(params[:pet])
-    if !params["owner"]["name"].empty?
-      @pet.owner = Owner.create(name: params["owner"]["name"])
+    if !params["owner_name"].empty?
+      @pet.owner = Owner.create(name: params["owner_name"])
     elsif !params["pet"]["owner_id"].empty?
       @pet.owner = Owner.find_by_id(id: params["pet"]["owner_id"])
     end
