884189cf318d68bd164389e25a4900c217b44103
Aaron Berns <anberns@gmail.com>
2019-01-30 21:37:18 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -30,7 +30,6 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do 
- 
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
