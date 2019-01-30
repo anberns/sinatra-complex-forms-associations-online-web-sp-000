401873493e1286844af12997cd37cb54edb91884
Aaron Berns <anberns@gmail.com>
2019-01-30 20:49:55 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -19,7 +19,7 @@ class OwnersController < ApplicationController
   end
 
   get '/owners/:id/edit' do 
-    @owner = Owner.find_by_id(params[:id])
+    @owner = Owner.find(params[:id])
     erb :'/owners/edit'
   end
 
